254ca0bf2559c9e716cc1b1fff4dbfad7fa6247a
4lejandroquintero <alejoq96@gmail.com>
2024-01-22 21:24:11 -0500

    ciclo de vida componentes ngonchanges

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
+  @Input({required: true}) duration = 0;
+  @Input({required: true}) message = '';
+
+  constructor() {
+    // NO ASYNC
+    // Before render
+    console.log('constructor');
+    console.log('-'.repeat(10))
+  }
+
+  ngOnChanges(changes: SimpleChange) {
+    // Before and during render
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+  }
 
 }
